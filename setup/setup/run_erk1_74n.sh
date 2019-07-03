eff451f8466210a23e471b7676e04386a3d9818f
Rafal P. Wiewiora <rafwiewiora@gmail.com>
2019-07-02 21:13:41 -0400

    add setup

---
+++ setup/setup/run_erk1_74n.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -J erk1_74n
+#BSUB -q gpuqueue
+#BSUB -n 1
+#BSUB -R span[ptile=1]
+#BSUB -R rusage[mem=10]
+#BSUB -gpu num=1
+#BSUB -W 48:00
+#BSUB -o /home/rafal.wiewiora/job_outputs/%J.stdout
+#BSUB -eo /home/rafal.wiewiora/job_outputs/%J.stderr
+
+cd $LS_SUBCWD
+python prep_equilibrate_phospho2.py ERK1_74N.pdb
